2f2f61c3b53ec94253d74ca11c41fa6bf9809fbc
Tilak <adireddytilak@gmail.com>
2025-10-28 13:01:52 +0530

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
+import Review from './Review'
+import React from 'react'
 
+const App = () => {
+    return(
+        <div>
+            <h1 id="review-heading">Our Reviews</h1>
+            <Review/>
+        </div>
+    )
+  
+}
+
+export default App
---
+++ src/components/Review.js
@@ -0,0 +1,77 @@
+import React,{useState} from 'react';
+const reviews=[
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+const Review=()=>{
+    const [index,setIndex]=useState(0);
+    const {id,name,job,image,text}=reviews[index];
+    const nextReview=()=>{
+        setIndex((prevIndex)=>(prevIndex+1)%reviews.length);
+    }
+    const prevReview=()=>{
+        setIndex((prevIndex)=>
+            prevIndex===0?reviews.length-1:prevIndex-1);
+    }
+    const randomReview = () => {
+    let randomIndex = Math.floor(Math.random() * reviews.length);
+    while (randomIndex === index) {
+      randomIndex = Math.floor(Math.random() * reviews.length);
+    }
+    setIndex(randomIndex);
+  };
+    return (
+    <div>
+        <h1 id="review-heading">Our reviews</h1>
+        {/* {reviews.map((review,index)=>( */}
+            <div className="review">
+            <img src={image} alt={name} className="person-img"/>
+            <h2 className="author">{name}</h2>
+            <h3 className="job">{job}</h3>
+            <p className="info">{text}</p>
+            </div>
+        {/* ))}  */}
+        <div className="button-container">
+            <button className="prev-btn" onClick={prevReview}>Previous</button>
+            <button className="next-btn" onClick={nextReview}>Next</button>
+            </div>   
+            <button className="random-btn" onClick={randomReview}>Surprise Me</button>
+    </div>
+  )
+}
+export default Review;
